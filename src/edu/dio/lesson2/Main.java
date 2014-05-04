b1f822c06cc06b6103aeb6b3c1e7a1c6dfe0ba48
Vladimir Kravchenko <vovchik33@gmail.com>
2014-05-04 16:13:14 +0300

    Comments for code were added

--- src/edu/dio/lesson2/Main.java
+++ src/edu/dio/lesson2/Main.java
@@ -9,14 +9,15 @@ public class Main {
         Person student1, student2, student3;
         Person worker1, worker2, worker3;
 
+        //objects initialization
         student1 = new Student("Vladimir", "Kravchenko", 32, Profession.STUDENT);
         student2 = new Student("Sergej", "Ivanchenko", 22, Profession.STUDENT);
         student3 = student1;
-
         worker1 = new Worker("Vladimir", "Kravchenko", 32, Profession.ENGINEER);
         worker2 = new Worker("Nikolaj", "Punchenko", 30, Profession.ARCHITECT);
         worker3 = worker1;
 
+        //data printing
         System.out.println(student1);
         System.out.println(student2);
         System.out.println(student3);
@@ -24,6 +25,7 @@ public class Main {
         System.out.println(worker2);
         System.out.println(worker3);
 
+        //objects comparison
         System.out.println(student1==student2);
         System.out.println(student1==student3);
         System.out.println(student1.equals(student3));
